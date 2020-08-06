38157dfe47c3e648151c010ffc305c835831141d
sungwookoo <sungwookoo.dev@gmail.com>
2020-08-06 18:05:08 +0900

    0806 파일업로드/다운로드, 조회수

--- src/main/java/com/sungwoo/board/BoardApplication.java
+++ src/main/java/com/sungwoo/board/BoardApplication.java
@@ -1,8 +1,13 @@
 package com.sungwoo.board;
 
+import com.sungwoo.board.domain.Board;
+import com.sungwoo.board.repository.BoardRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class BoardApplication {
